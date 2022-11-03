297a1e22ea5eb8b4db13753a486570f26ca370af
Ilya Navumenka <navumenkawork@gmail.com>
2022-11-03 02:10:58 -0700

    feat: add webpack confiq

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,71 @@
-const path = require('path');
+// const path = require('path');
 
-module.exports = {
-  entry: './src/index.js',
-  output: {
-    path: path.resolve(__dirname, 'dist'),
-    filename: 'main.js',
-  },
-};
+// module.exports = {
+//   entry: './src/index.js',
+//   output: {
+//     path: path.resolve(__dirname, 'dist'),
+//     filename: 'main.js',
+//   },
+// };
+
+const webpack = require('webpack');
+const path = require('path');
+const { CleanWebpackPlugin } = require('clean-webpack-plugin');
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = (env, options) => {
+  const isProduction = options.mode === 'production';
+
+  const config = {
+    mode: isProduction ? 'production' : 'development',
+    devtool: isProduction ? false : 'source-map',
+    watch: !isProduction,
+    entry: ['./src/index.js', './src/sass/style.scss'],
+    output: {
+      path: path.join(__dirname, '/dist'),
+      filename: 'script.js',
+    },
+
+    module: {
+      rules: [
+        {
+          test: /\.js$/,
+          exclude: /node_modules/,
+          use: {
+            loader: 'babel-loader',
+            options: {
+              presets: ['@babel/preset-env']
+            }
+          }
+        }, 
+
+        {
+          test: /\.html$/i,
+          loader: "html-loader",
+        },
+        
+        {
+          test: /\.scss$/,
+          use: [
+            MiniCssExtractPlugin.loader, 'css-loader', 'sass-loader'
+        ]
+        }
+      ]
+    },
+
+    
+
+    plugins: [
+      new CleanWebpackPlugin(),
+      new HtmlWebpackPlugin({
+        template: 'index.html'
+      }),
+      new MiniCssExtractPlugin({
+          filename: 'style.css'
+      })
+  ]
+  }
+
+  return config;
+}
